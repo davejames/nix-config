9de56a98528c4231a645669b20ffd066983face8
David James <davejames@gmail.com>
2023-04-03 11:14:42 +1000

    add startup script

---
+++ .config/qtile/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Startup script for qtile
+#pulseaudio -D
+#emacs --daemon &
+#syncthing &
+#~/.fehbg &
+#xmodmap ~/.Xmodmap
+#picom -b
+#/usr/lib/kdeconnectd &
+#/usr/bin/kdeconnect-indicator &
+#redshift-gtk &
+nm-applet &
+#gnome-keyring-daemon --start
+#nextcloud &
+#keepassxc &
