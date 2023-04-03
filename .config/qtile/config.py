226eb5c0df2538e25b0191fdcd9ff00018c976e9
David James <davejames@gmail.com>
2023-04-03 11:13:09 +1000

    update qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -25,13 +25,42 @@
 # SOFTWARE.
 
 import subprocess
+import os
 
-from libqtile import bar, layout, widget
+from libqtile import bar, layout, hook, qtile
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
+from libqtile.layout import RatioTile
+
+from qtile_extras import widget
+from qtile_extras.widget.decorations import BorderDecoration
 
 mod = "mod4"
-terminal = "alacritty"
+terminal = "kitty"
+
+
+@lazy.function
+def get_grow_method(direction, *args):
+    current_layout = qtile.current_layout
+    if isinstance(current_layout, RatioTile):
+        if direction == "left":
+            return current_layout.cmd_grow_left
+        elif direction == "right":
+            return current_layout.cmd_grow_right
+        elif direction == "up":
+            return current_layout.cmd_grow_up
+        elif direction == "down":
+            return current_layout.cmd_grow_down
+    else:
+        if direction == "left":
+            return lazy.layout.grow_left()
+        elif direction == "right":
+            return lazy.layout.grow_right()
+        elif direction == "up":
+            return lazy.layout.grow_up()
+        elif direction == "down":
+            return lazy.layout.grow_down()
+
 
 keys = [
     # A list of available commands that can be bound to keys can be found
@@ -44,14 +73,27 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
+    # Key([mod, "control"], "h", get_grow_method("left"), desc="Grow window to the left"),
+    # Key([mod, "control"], "l", get_grow_method("right"), desc="Grow window to the right"),
+    # Key([mod, "control"], "j", get_grow_method("down"), desc="Grow window down"),
+    # Key([mod, "control"], "k", get_grow_method("up"), desc="Grow window up"),
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -72,6 +114,10 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+
+    # Custom keybindings
+    Key([mod, "shift"], "l", lazy.spawn("betterlockscreen -l"), desc="Lock screen"),
+
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -100,61 +146,187 @@ for i in groups:
         ]
     )
 
+default_margin = 4
+default_border_width = 2
+
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
+    layout.Columns(
+        border_focus_stack=["#d75f5f", "#8f3d3d"],
+        border_width=default_border_width,
+        margin=default_margin,
+    ),
+    layout.Max(margin=default_margin, border_width=default_border_width),
     # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Stack(num_stacks=2, border_width=default_border_width),
+    layout.Bsp(margin=default_margin, border_width=default_border_width),
+    layout.Matrix(margin=default_margin, border_width=default_border_width),
+    layout.MonadTall(border_width=default_border_width),
+    layout.MonadWide(border_width=default_border_width),
+    layout.RatioTile(margin=default_margin, border_width=default_border_width),
+    layout.Tile(margin=default_margin, border_width=default_border_width),
+    layout.TreeTab(margin=default_margin, border_width=default_border_width),
+    layout.VerticalTile(margin=default_margin, border_width=default_border_width),
+    layout.Zoomy(margin=default_margin, border_width=default_border_width),
+]
+
+colours = [
+    ["#282c34", "#282c34"],
+    ["#1c1f24", "#1c1f24"],
+    ["#dfdfdf", "#dfdfdf"],
+    ["#ff6c6b", "#ff6c6b"],
+    ["#98be65", "#98be65"],
+    ["#da8548", "#da8548"],
+    ["#51afef", "#51afef"],
+    ["#c678dd", "#c678dd"],
+    ["#46d9ff", "#46d9ff"],
+    ["#a9a1e1", "#a9a1e1"],
 ]
 
 widget_defaults = dict(
-    font="sans",
-    fontsize=12,
-    padding=3,
+    font="Ubuntu Bold", fontsize=12, padding=3, background=colours[1]
 )
+
 extension_defaults = widget_defaults.copy()
 
 # Use xrandr output to see what monitors are available
 xrandr_output = subprocess.check_output("xrandr").decode()
-connected_monitors = [line.split()[0] for line in xrandr_output.splitlines() if " connected" in line]
+connected_monitors = [
+    line.split()[0] for line in xrandr_output.splitlines() if " connected" in line
+]
+primary_line = [line for line in xrandr_output.splitlines() if "primary" in line]
+primary_monitor = primary_line and primary_line[0].split()[0]
 
 # Add a status bar to each monitor
 screens = []
 
-for connected_monitor in connected_monitors:
+
+def generate_bar_elements(monitor_name, show_systray):
+    bar_elements = [
+        widget.Image(
+            filename="~/.config/qtile/icons/terminal.png",
+            scale="True",
+            mouse_callbacks={"Button1": lambda: qtile.cmd_spawn(terminal)},
+        ),
+        widget.CurrentLayout(),
+        widget.GroupBox(),
+        widget.Prompt(),
+        widget.WindowName(),
+        widget.Chord(
+            chords_colors={
+                "launch": ("#ff0000", "#ffffff"),
+            },
+            name_transform=lambda name: name.upper(),
+        ),
+        widget.TextBox(
+            monitor_name,
+            name="monitor",
+            foreground=colours[1],
+            background=colours[7 if primary_monitor else 2],
+            mouse_callbacks={"Button1": lambda: qtile.cmd_spawn("arandr")},
+
+        ),
+        widget.Sep(
+            linewidth=0,
+            padding=6,
+            foreground=colours[1],
+            background=colours[1],
+        ),
+        # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+        widget.Systray(icon_size=20)
+        if show_systray
+        else widget.Sep(
+            linewidth=0,
+            padding=0,
+            foreground=colours[1],
+            background=colours[1],
+        ),
+        # widget.Systray() if show_systray else None,
+        widget.Sep(
+            linewidth=0,
+            padding=6,
+            foreground=colours[1],
+            background=colours[1],
+        ),
+        widget.Clock(
+            format="%Y-%m-%d %a %I:%M %p",
+            foreground=colours[4],
+            background=colours[0],
+        ),
+        widget.Sep(
+            linewidth=0,
+            padding=6,
+            foreground=colours[1],
+            background=colours[1],
+        ),
+        # widget.Wlan(
+        #     interface="wlo1",
+        #     format="Wifi {down} ↓↑ {up}",
+        #     foreground=colours[5],
+        #     background=colours[0],
+        #     padding=5,
+        #     decorations=[
+        #         BorderDecoration(
+        #             colour=colours[6],
+        #             border_width=[0, 0, 2, 0],
+        #             padding_x=5,
+        #             padding_y=None,
+        #         )
+        #     ],
+        # ),
+        widget.Net(
+            interface="wlo1",
+            format="Wifi {down} ↓↑ {up}",
+            foreground=colours[5],
+            background=colours[0],
+            padding=5,
+            decorations=[
+                BorderDecoration(
+                    colour=colours[6],
+                    border_width=[0, 0, 2, 0],
+                    padding_x=5,
+                    padding_y=None,
+                )
+            ],
+        ),
+        widget.Sep(
+            linewidth=0,
+            padding=6,
+            foreground=colours[1],
+            background=colours[1],
+        ),
+        widget.Battery(
+            foreground=colours[6],
+            background=colours[0],
+        ),
+        widget.Sep(
+            linewidth=0,
+            padding=6,
+            foreground=colours[1],
+            background=colours[1],
+        ),
+        widget.QuickExit(
+            foreground=colours[7],
+            background=colours[0],
+        ),
+    ]
+    # if not show_systray:
+    #     bar_elements.pop(-10)
+    return bar_elements
+
+
+for i, connected_monitor in enumerate(connected_monitors):
+    if primary_monitor:
+        show_systray = connected_monitor == primary_monitor
+    else:
+        show_systray = i == 0
     screens.append(
         Screen(
             bottom=bar.Bar(
-                [
-                    widget.CurrentLayout(),
-                    widget.GroupBox(),
-                    widget.Prompt(),
-                    widget.WindowName(),
-                    widget.Chord(
-                        chords_colors={
-                            "launch": ("#ff0000", "#ffffff"),
-                        },
-                        name_transform=lambda name: name.upper(),
-                    ),
-                    widget.TextBox("default config", name="default"),
-                    widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                    # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                    # widget.StatusNotifier(),
-                    widget.Systray(),
-                    widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                    widget.QuickExit(),
-                ],
+                generate_bar_elements(
+                    monitor_name=connected_monitor, show_systray=show_systray
+                ),
                 24,
-                monitor = connected_monitor
+                monitor=connected_monitor
                 # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
                 # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
             ),
@@ -163,8 +335,15 @@ for connected_monitor in connected_monitors:
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -205,3 +384,11 @@ wl_input_rules = None
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
+
+
+@hook.subscribe.startup_once
+def start_once():
+    """Run after qtile is started"""
+
+    autostart_file = os.path.expanduser("~/.config/qtile/startup.sh")
+    subprocess.call([autostart_file])
