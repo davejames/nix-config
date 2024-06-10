d7b14b199687e6590f9886d1812763ee38862075
David James <david.james@willdooit.com>
2024-06-10 21:19:24 +1000

    test

--- hosts/canary/hardware-configuration.nix
+++ hosts/canary/hardware-configuration.nix
@@ -7,23 +7,23 @@
   system,
   ...
 }: {
-  boot = {
-    };
-    # loader = {
-    #   generationsDir.copyKernels = true;
-    #   efi = {
-    #     canTouchEfiVariables = false;
-    #   };
-    #   grub = {
-    #     enable = true;
-    #     useOSProber = true;
-    #     copyKernels = true;
-    #     efiSupport = true;
-    #     device = "nodev";
-    #     zfsSupport = true;
-    #     efiInstallAsRemovable = true;
-    #   };
-    # };
-  };
+  # boot = {
+  #   };
+  #   # loader = {
+  #   #   generationsDir.copyKernels = true;
+  #   #   efi = {
+  #   #     canTouchEfiVariables = false;
+  #   #   };
+  #   #   grub = {
+  #   #     enable = true;
+  #   #     useOSProber = true;
+  #   #     copyKernels = true;
+  #   #     efiSupport = true;
+  #   #     device = "nodev";
+  #   #     zfsSupport = true;
+  #   #     efiInstallAsRemovable = true;
+  #   #   };
+  #   # };
+  # };
 }
 
